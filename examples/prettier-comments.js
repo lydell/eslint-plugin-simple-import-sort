1abf8e423a060116226cdde7bacca0ca5ab2fbc2
Simon Lydell <simon.lydell@gmail.com>
2018-11-18 16:57:02 +0100

    Add one more test for Prettier

---
+++ examples/prettier-comments.js
@@ -0,0 +1,14 @@
+/* eslint prettier/prettier: "error" */
+// This is just a test to make sure that this plugin plays well with Prettier.
+
+import {/* start */ b /*b*/, a /*a*/} from "t"
+
+import def, { // start
+  // f
+  f, // f
+  d /*d*/,
+  e, /*c
+  */
+  c
+  // end
+} from "s"
