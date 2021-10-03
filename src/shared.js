b750af608a813b4375b6b7c9a5af4f0ec20eae3e
Ameet Kaustav <akaustav@users.noreply.github.com>
2021-10-03 07:24:57 -0700

    Best case ASCII sorting so far

--- src/shared.js
+++ src/shared.js
@@ -805,19 +805,19 @@ function getSource(node) {
       .replace(/^[./]*\.$/, "$&/")
       // Make `../` sort after `../../` but before `../a` etc.
       // Why a comma? See the next comment.
-      .replace(/^[./]*\/$/, "$&,")
+      .replace(/^[./]*\//, "$&,")
       // Make `.` and `/` sort before any other punctation.
       // The default order is: _ - , x x x . x x x / x x x
       // We’re changing it to: . / , x x x _ x x x - x x x
       .replace(/[./_-]/g, (char) => {
         switch (char) {
-          case ".":
-            return "_";
-          case "/":
-            return "-";
-          case "_":
-            return ".";
           case "-":
+            return "_";
+          case ".":
+            return "-";
+          case "/":
+            return ".";
+          case "_":
             return "/";
           // istanbul ignore next
           default:
