8ee1d495dacf75838de859f7aaa2ae2be4f894ee
Simon Lydell <simon.lydell@gmail.com>
2022-09-03 10:59:25 +0200

    Update more examples based on the defaults

--- examples/groups.default-reverse.js
+++ examples/groups.default-reverse.js
@@ -1,4 +1,7 @@
 import "./polyfills";
+import * as fs from "node:fs";
+import fs2 from "fs";
+import forge from "node-forge";
 import react from "react";
 import { storiesOf } from "@storybook/react";
 import App from "@/App";
