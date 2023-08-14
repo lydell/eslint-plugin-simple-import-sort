1477e9a67312ee63cbafc2e950b1c916b81d72ff
Patrick Eriksson <paleite@users.noreply.github.com>
2023-08-14 14:25:02 +0200

    Add recommended config

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ const importsRule = require("./imports");
 const exportsRule = require("./exports");
 
 module.exports = {
+  configs: {
+    recommended: {
+      parserOptions: {
+        sourceType: "module",
+      },
+      plugins: ["simple-import-sort"],
+      rules: {
+        "simple-import-sort/imports": "error",
+        "simple-import-sort/exports": "error",
+      },
+    },
+  },
   rules: {
     imports: importsRule,
     exports: exportsRule,
