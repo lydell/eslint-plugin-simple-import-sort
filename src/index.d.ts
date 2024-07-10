dd53d5db4c591f8128f56d37bda30f13b00bb78e
Eli <88557639+lishaduck@users.noreply.github.com>
2024-07-10 13:49:21 -0500

    chore: ship types for configs

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,3 +3,25 @@ import type { ESLint } from "eslint";
 declare const eslintPluginSimpleImportSort: ESLint.Plugin;
 
 export = eslintPluginSimpleImportSort;
+
+declare module "eslint-define-config" {
+  export interface CustomRuleOptions {
+    /**
+     * Automatically sort imports.
+     *
+     * @see [imports](https://github.com/lydell/eslint-plugin-simple-import-sort/blob/main/docs/rules/imports.md)
+     */
+    "simple-import-sort/imports": [
+      {
+        groups?: (RegExp | string)[][];
+      },
+    ];
+
+    /**
+     * Automatically sort exports.
+     *
+     * @see [exports](https://github.com/lydell/eslint-plugin-simple-import-sort/blob/main/docs/rules/exports.md)
+     */
+    "simple-import-sort/exports": [];
+  }
+}
